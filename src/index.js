f2e999fca240359f9adb86c5b6913e0711b0a3a5
Prashwar Mishra <prashwarmishra@gmail.com>
2021-05-26 20:07:38 +0530

    install redux and initialize store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('store', store);
+console.log('store state', store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
