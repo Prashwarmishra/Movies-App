5d9b1ba8c24ace51619eea327ed29cf8b38aa4f2
Prashwar Mishra <prashwarmishra@gmail.com>
2021-05-26 19:35:31 +0530

    add actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,28 @@
-import React from 'react';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import {data} from '../data';
 
-class App extends React.Component {
-  render(){
-    return (
-      <div className="App">
-        <Navbar />
-        <div className = 'main'>
-          <div className = 'tabs'>
-            <div className = 'tab'>Movies</div>
-            <div className = 'tab'>Favourites</div>
-          </div>
-  
-          <div className = 'list'>
-            {
-              data.map((movie) => (
-                <MovieCard movie = {movie} />
-              ))
-            }
-          </div>
-  
+function App(){
+  return (
+    <div className="App">
+      <Navbar />
+      <div className = 'main'>
+        <div className = 'tabs'>
+          <div className = 'tab'>Movies</div>
+          <div className = 'tab'>Favourites</div>
         </div>
+
+        <div className = 'list'>
+          {
+            data.map((movie) => (
+              <MovieCard movie = {movie} />
+            ))
+          }
+        </div>
+
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
