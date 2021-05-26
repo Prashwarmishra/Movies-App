4908b6270f2f9a86acfc2efa4e157416d50f3c5b
Prashwar Mishra <prashwarmishra@gmail.com>
2021-05-26 17:18:18 +0530

    add and style the components: Navbar, main and MovieCard

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,31 @@
+import React from 'react';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
+import {data} from '../data';
 
-
-function App() {
-  return (
-    <div className="App">
-      My movie app
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <Navbar />
+        <div className = 'main'>
+          <div className = 'tabs'>
+            <div className = 'tab'>Movies</div>
+            <div className = 'tab'>Favourites</div>
+          </div>
+  
+          <div className = 'list'>
+            {
+              data.map((movie) => (
+                <MovieCard movie = {movie} />
+              ))
+            }
+          </div>
+  
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
