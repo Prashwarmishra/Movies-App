9e03bd2e80d4b6982840acf479af9dc5128d4016
Prashwar Mishra <prashwarmishra@gmail.com>
2021-05-29 22:55:11 +0530

    set up context api

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import MovieCard from './MovieCard';
 import {data} from '../data';
 import { addMovies, showFavourites } from '../actions';
+import { StoreContext } from '../index';
 
 class App extends React.Component{
 
@@ -44,33 +45,39 @@ class App extends React.Component{
     const moviesList = showFavourites ? favourites : list;
     
     return (
-      <div className="App">
-        <Navbar 
-          dispatch = { store.dispatch } 
-          search = { search }
-        />
-        <div className = 'main'>
-          <div className = 'tabs'>
-            <div className = {`tab ${showFavourites ? '' : 'active-tabs'}`} onClick = {() => this.onChangeTab(false)}>Movies</div>
-            <div className = {`tab ${showFavourites ? 'active-tabs' : ''}`} onClick = {() => this.onChangeTab(true)}>Favourites</div>
-          </div>
-  
-          <div className = 'list'>
-            {
-              moviesList.map((movie, index) => (
-                <MovieCard 
-                  movie = {movie} 
-                  key = {`movie-id-${index}`}
-                  dispatch = {store.dispatch}
-                  isMovieFavourite = {this.isMovieFavourite(movie)}
-                />
-              ))
-            }
-          </div>
-          { moviesList.length <= 0 ? <div className = 'no-movies' > No movies to show </div> : null }
-        </div>
-      </div>
-    );
+      <StoreContext.Consumer>
+        {(store) => {
+          return (
+            <div className="App">
+              <Navbar 
+                dispatch = { store.dispatch } 
+                search = { search }
+              />
+              <div className = 'main'>
+                <div className = 'tabs'>
+                  <div className = {`tab ${showFavourites ? '' : 'active-tabs'}`} onClick = {() => this.onChangeTab(false)}>Movies</div>
+                  <div className = {`tab ${showFavourites ? 'active-tabs' : ''}`} onClick = {() => this.onChangeTab(true)}>Favourites</div>
+                </div>
+        
+                <div className = 'list'>
+                  {
+                    moviesList.map((movie, index) => (
+                      <MovieCard 
+                        movie = {movie} 
+                        key = {`movie-id-${index}`}
+                        dispatch = {store.dispatch}
+                        isMovieFavourite = {this.isMovieFavourite(movie)}
+                      />
+                    ))
+                  }
+                </div>
+                { moviesList.length <= 0 ? <div className = 'no-movies' > No movies to show </div> : null }
+              </div>
+            </div>
+          );
+        }}
+      </StoreContext.Consumer>
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
@@ -25,9 +25,24 @@ const logger = ( {dispatch, getState} ) => (next) => (action) => {
 
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 
+export const StoreContext = createContext();
+
+console.log('StoreContext=', StoreContext);
+
+class Provider extends React.Component{
+  render(){
+    const { store } = this.props;
+    return (
+      <StoreContext.Provider value = { store }>
+        { this.props.children }
+      </StoreContext.Provider>
+    )
+  }
+}
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store} >
     <App store = {store} />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
