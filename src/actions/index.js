cce2e31f512bd3fbfcbfe25cd35b91ef3dd4fb6f
prashwar_mishra <prashwarmishra@gmail.com>
2021-06-30 16:28:37 +0530

    change http to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,64 +1,63 @@
-
 //action types
-export const ADD_MOVIES = 'ADD_MOVIES';
-export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
-export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
-export const SHOW_FAVOURITES = 'SHOW_FAVOURITES';
-export const ADD_TO_MOVIES = 'ADD_TO_MOVIES';
-export const ADD_MOVIES_FROM_SEARCH_RESULT = 'ADD_MOVIES_FROM_SEARCH_RESULT';
+export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_TO_FAVOURITES = "ADD_TO_FAVOURITES";
+export const REMOVE_FROM_FAVOURITES = "REMOVE_FROM_FAVOURITES";
+export const SHOW_FAVOURITES = "SHOW_FAVOURITES";
+export const ADD_TO_MOVIES = "ADD_TO_MOVIES";
+export const ADD_MOVIES_FROM_SEARCH_RESULT = "ADD_MOVIES_FROM_SEARCH_RESULT";
 
 //action creators
-export function addMovies(movies){
-    return {
-        type: ADD_MOVIES,
-        movies
-    }
+export function addMovies(movies) {
+  return {
+    type: ADD_MOVIES,
+    movies,
+  };
 }
 
-export function addToFavourites(movie){
-    return {
-        type: ADD_TO_FAVOURITES,
-        movie
-    }
+export function addToFavourites(movie) {
+  return {
+    type: ADD_TO_FAVOURITES,
+    movie,
+  };
 }
 
-export function removeFromFavourites(movie){
-    return {
-        type: REMOVE_FROM_FAVOURITES,
-        movie
-    }
+export function removeFromFavourites(movie) {
+  return {
+    type: REMOVE_FROM_FAVOURITES,
+    movie,
+  };
 }
 
-export function showFavourites(val){
-    return {
-        type: SHOW_FAVOURITES,
-        val
-    }
+export function showFavourites(val) {
+  return {
+    type: SHOW_FAVOURITES,
+    val,
+  };
 }
 
-export function addToMovies(movie){
-    return {
-        type: ADD_TO_MOVIES,
-        movie
-    }
+export function addToMovies(movie) {
+  return {
+    type: ADD_TO_MOVIES,
+    movie,
+  };
 }
 
-export function handleMovieSearch(searchText){
-    const url = `http://www.omdbapi.com/?apikey=6d404629&t=${searchText}`;
+export function handleMovieSearch(searchText) {
+  const url = `https://www.omdbapi.com/?apikey=6d404629&t=${searchText}`;
 
-    return function(dispatch){
-        fetch(url)
-        .then(response => response.json())
-        .then((movie) => {
-            console.log('movie-->', movie);
-            dispatch(addMovieFromSearchResult(movie))
-        })
-    }
+  return function (dispatch) {
+    fetch(url)
+      .then((response) => response.json())
+      .then((movie) => {
+        console.log("movie-->", movie);
+        dispatch(addMovieFromSearchResult(movie));
+      });
+  };
 }
 
-export function addMovieFromSearchResult(movie){
-    return {
-        type: ADD_MOVIES_FROM_SEARCH_RESULT,
-        movie
-    }
+export function addMovieFromSearchResult(movie) {
+  return {
+    type: ADD_MOVIES_FROM_SEARCH_RESULT,
+    movie,
+  };
 }
